8ab607a6bb7d57c3e38dec9383a3368b2e5294d5
Oleh Savenko <osavenko@gmail.com>
2024-07-27 00:49:48 +0300

    task02

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,38 +3,37 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = false,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        roleName = "hello_world-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
+@LambdaUrlConfig(authType = AuthType.NONE)
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		final Map<String, String> input = (Map<String, String>) request;
-		final String path = input.get("rawPath");
+    public Map<String, Object> handleRequest(Object request, Context context) {
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        final HashMap<String, Object> input = (HashMap<String, Object>) request;
+        final String path = input.get("rawPath").toString();
+        if ("/hello".equals(path)) {
+            resultMap.put("statusCode", 200);
+            resultMap.put("body", "{\"statusCode\":200, \"message\": \"Hello from Lambda\"}");
+            return resultMap;
+        }
+        Map<String, Object> requestContext = (((Map<String, Map<String, Object>>)request).get("requestContext"));
+        Map<String, Object> http = (Map<String, Object>) requestContext.get("http");
 
-		//System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		if(path.equals("/hello")) {
-			resultMap.put("statusCode", 200);
-			resultMap.put("message", "Hello from Lambda");
-			return resultMap;
-		}
+        String message = "Bad request syntax or unsupported method. Request path: " + http.get("path") + ". HTTP method: " + http.get("method");
 
-		Map<String, Object> requestContext = (((Map<String, Map<String, Object>>)request).get("requestContext"));
-		Map<String, Object> http = (Map<String, Object>) requestContext.get("http");
-
-		resultMap.put("statusCode", 400);
-		resultMap.put("message",
-				"Bad request syntax or unsupported method. Request path: "+path
-						+". HTTP method: "+http.get("path"));
-
-		return resultMap;
-	}
+        resultMap.put("statusCode", 400);
+        resultMap.put("body",  "{\"statusCode\":400, \"message\": \"" + message + "\"}");
+        return resultMap;
+    }
 }

f2a4e471ae1c663a53e0e3762dbe9c8551455756
Oleh Savenko <osavenko@gmail.com>
2024-08-02 20:02:32 +0300

    task04 sqs sns

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -2,6 +2,7 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
@@ -10,15 +11,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "sqs_handler",
-	roleName = "sqs_handler-role",
-	isPublishVersion = false,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        roleName = "sqs_handler-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @SqsTriggerEventSource(targetQueue = "async_queue", batchSize = 5)
-public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
+public class SqsHandler implements RequestHandler<SQSEvent, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		return null;
-	}
+    public Map<String, Object> handleRequest(SQSEvent event, Context context) {
+        context.getLogger().log("Event has " + event.getRecords().size() + " msg");
+        event.getRecords().forEach(msg -> context.getLogger().log(msg.getBody()));
+        return null;
+
+    }
 }

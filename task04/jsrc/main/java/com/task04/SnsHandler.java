b153990be7693200074da550090f2ada02ecb677
Oleh Savenko <osavenko@gmail.com>
2024-08-02 21:23:54 +0300

    task04 sqs sns

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -1,24 +1,40 @@
 package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @LambdaHandler(lambdaName = "sns_handler",
-	roleName = "sns_handler-role",
-	isPublishVersion = false,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        roleName = "sns_handler-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @SnsEventSource(targetTopic = "lambda_topic")
-public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
+@DependsOn(
+        name = "lambda_topic",
+        resourceType = ResourceType.SNS_TOPIC
+)
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		return null;
-	}
+public class SnsHandler implements RequestHandler<SNSEvent, List<String>> {
+
+    public List<String> handleRequest(SNSEvent event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("EVENT TYPE: " + event.getClass().toString());
+        var messagesFound = new ArrayList<String>();
+        for (SNSEvent.SNSRecord record : event.getRecords()) {
+            SNSEvent.SNS msg = record.getSNS();
+            logger.log("sns message: " + msg.getMessage());
+            messagesFound.add(msg.getMessage());
+        }
+        return messagesFound;
+    }
 }
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -1,14 +1,17 @@
 package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @LambdaHandler(lambdaName = "sqs_handler",
         roleName = "sqs_handler-role",
@@ -16,12 +19,21 @@ import java.util.Map;
         logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @SqsTriggerEventSource(targetQueue = "async_queue", batchSize = 5)
-public class SqsHandler implements RequestHandler<SQSEvent, Map<String, Object>> {
+@DependsOn(
+        name = "async_queue",
+        resourceType = ResourceType.SQS_QUEUE
+)
 
-    public Map<String, Object> handleRequest(SQSEvent event, Context context) {
-        context.getLogger().log("Event has " + event.getRecords().size() + " msg");
-        event.getRecords().forEach(msg -> context.getLogger().log(msg.getBody()));
-        return null;
+public class SqsHandler implements RequestHandler<SQSEvent, List<String>> {
 
+    public List<String> handleRequest(SQSEvent event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("EVENT TYPE: " + event.getClass().toString());
+        var messagesFound = new ArrayList<String>();
+        for (SQSEvent.SQSMessage msg : event.getRecords()) {
+            logger.log("sqs message: " + msg.getBody());
+            messagesFound.add(msg.getBody());
+        }
+        return messagesFound;
     }
 }

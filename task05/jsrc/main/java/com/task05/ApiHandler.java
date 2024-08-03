160b179d4a7bee79aa326b39a72c8950aa15eb6b
Oleh Savenko <osavenko@gmail.com>
2024-08-03 22:19:49 +0300

    task05 DynamoDB

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -22,11 +22,7 @@ public class ApiHandler implements RequestHandler<Map<String, Object>, Map<Strin
     private final String tableName;
     private final DynamoDbClient db = DynamoDbClient.builder().build();
     public ApiHandler(){
-        tableName = "cmtr-2c83ab08-Events-test";
-/*
-        tableName = !System.getenv(TABLE_NAME_ENV).equals("${target_table}")
-                ? System.getenv(TABLE_NAME_ENV) : "cmtr-2c83ab08-Events-test";
-*/
+        tableName = System.getenv(TABLE_NAME_ENV);
     }
     public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
         Event event = parseEvent(request);

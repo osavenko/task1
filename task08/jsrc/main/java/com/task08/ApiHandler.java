20abbd9362ce295ce7a955a035f537058fb29dda
Oleh Savenko <osavenko@gmail.com>
2024-08-08 00:32:48 +0300

    task08 Lambda+Layers

---
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -0,0 +1,65 @@
+package com.task08;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.google.gson.Gson;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "api_handler",
+        roleName = "api_handler-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
+        layers = {"sdk-layer"}
+)
+@LambdaLayer(
+        layerName = "sdk-layer",
+        libraries = {"lib/gson-2.10.1.jar"},
+        runtime = DeploymentRuntime.JAVA11,
+        artifactExtension = ArtifactExtension.ZIP
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
+)
+
+public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, String> {
+
+    public String  handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+        Gson gson = new Gson();
+        try {
+            URL url = new URL("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+
+                reader.lines().forEach(response::append);
+                reader.close();
+
+                return String.valueOf(response);
+            }
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        return "";
+    }
+}

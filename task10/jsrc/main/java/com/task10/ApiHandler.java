dbf1ba55fab1f7539d72b27de08150d4f7a9283d
Oleh Savenko <osavenko@gmail.com>
2024-08-10 12:12:47 +0300

    task10 Cognito

---
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -0,0 +1,39 @@
+package com.task10;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+@LambdaHandler(lambdaName = "api_handler",
+        roleName = "api_handler-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, String> {
+
+    public String handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+        String path = request.getPath();
+        final LambdaLogger logger = context.getLogger();
+
+        logger.log("Current request :" + request);
+        logger.log("Current path: " + path);
+        logger.log("Current method: " + request.getHttpMethod());
+        logger.log("Current body: " + request.getBody());
+        switch (path) {
+            case Task10Util.Path.SING_IN:
+                logger.log("Singin operation ");
+            case Task10Util.Path.SING_UP:
+                logger.log("Singup operation ");
+            case Task10Util.Path.TABLES:
+                logger.log("Tables operation ");
+            case Task10Util.Path.RESERVATIONS:
+                logger.log("Reservation operation ");
+        }
+		logger.log("Unknown path");
+        return path;
+    }
+}
---
+++ task10/jsrc/main/java/com/task10/Task10Util.java
@@ -0,0 +1,10 @@
+package com.task10;
+
+public class Task10Util {
+    public static class Path{
+        public static final String SING_UP = "/singup";
+        public static final String SING_IN = "/singin";
+        public static final String TABLES = "/tables";
+        public static final String RESERVATIONS = "/reservations";
+    }
+}

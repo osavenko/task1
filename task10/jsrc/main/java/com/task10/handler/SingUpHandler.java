41896af388ff2e539a38fc02aecc06a7137a0e00
Oleh Savenko <osavenko@gmail.com>
2024-08-11 20:31:46 +0300

    task10 Cognito

--- task10/jsrc/main/java/com/task10/handler/SingUpHandler.java
+++ task10/jsrc/main/java/com/task10/handler/SingUpHandler.java
@@ -20,23 +20,27 @@ public class SingUpHandler {
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
         final LambdaLogger logger = context.getLogger();
+        logger.log("SING UP(inside)");
+        logger.log("SING UP(inside) Method" + request.getHttpMethod());
+        logger.log("SING UP(inside) Body"+ request.getBody());
 
-/*        if (!request.getHttpMethod().equals(HttpMethod.POST)) {
-            //logger.log("Incorrect method, need POST current: " + request.getHttpMethod());
+        if (!request.getHttpMethod().equals(HttpMethod.POST)) {
+            logger.log("Incorrect method, need POST current: " + request.getHttpMethod());
 
             response.setStatusCode(StatusCode.BAD_REQUEST);
             return response;
-        }*/
+        }
 
+        logger.log("SING UP(inside, before try) Body"+ request.getBody());
         try {
             Map<String, String> singUpBody = new ObjectMapper().readValue(request.getBody(), Map.class);
             SingUp singUp = SingUp.getInstance(singUpBody);
 
-/*
-            logger.log("SingUp body: " + request.getBody());
-            logger.log("SingUp object: " + singUpBody.toString());
-*/
 
+            logger.log("SING UP(inside, try)SingUp body: " + request.getBody());
+            logger.log("SING UP(inside, try)SingUp object: " + singUpBody.toString());
+
+            logger.log("SING UP(inside, try) Create SingUpRequest");
             SignUpRequest signUpRequest = new SignUpRequest()
                     .withClientId(getUserClientId())
                     .withUsername(singUp.getEmail())
@@ -48,20 +52,27 @@ public class SingUpHandler {
                                     .withValue(singUp.getLastName()),
                             new AttributeType().withName(SingUpAttributesName.EMAIL)
                                     .withValue(singUp.getEmail()));
-
+            logger.log("SING UP(inside, try) SingUpRequest was created");
+            logger.log("SING UP(inside, try) Create Cognito client");
             AWSCognitoIdentityProvider cognitoClient = AWSCognitoIdentityProviderClientBuilder.defaultClient();
-            cognitoClient.signUp(signUpRequest);
+            logger.log("SING UP(inside, try) Cognito client was created");
 
+            cognitoClient.signUp(signUpRequest);
+            logger.log("SING UP(inside, try) Cognito client  singUp was called");
+            logger.log("SING UP(inside, try) Create AdminConfirmSignUpRequest");
             AdminConfirmSignUpRequest adminConfirm = new AdminConfirmSignUpRequest()
                     .withUsername(singUp.getEmail())
                     .withUserPoolId(getUserPoolId());
+            logger.log(adminConfirm.getUserPoolId());
 
+            logger.log("SING UP(inside, try) AdminConfirmSignUpRequest was created");
+            logger.log("SING UP(inside, try) Call AdminConfirmSignUpRequest adminConfirmSignUp");
             cognitoClient.adminConfirmSignUp(adminConfirm);
-
+            logger.log("SING UP(inside, try) AdminConfirmSignUpRequest adminConfirmSignUp was called");
             response.setStatusCode(StatusCode.SUCCESS);
             return response;
         } catch (Exception e) {
-//            logger.log("Exception: " + e.getMessage());
+            logger.log("Exception: " + e.getMessage());
 
             response.setStatusCode(StatusCode.BAD_REQUEST);
             return response;

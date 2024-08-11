f8082e077698a3142d2e946c2d831be426bbe1a2
Oleh Savenko <osavenko@gmail.com>
2024-08-11 21:17:07 +0300

    task10 Cognito

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -25,26 +25,36 @@ public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, A
         String path = request.getPath();
         final LambdaLogger logger = context.getLogger();
 
-        logger.log("Current request :" + request);
-        logger.log("Current path: " + path);
-        logger.log("Current method: " + request.getHttpMethod());
-        logger.log("Current body: " + request.getBody());
+        logger.log("=========Current request :" + request);
+        logger.log("=========Current path: " + path);
+        logger.log("=========Current method: " + request.getHttpMethod());
+        logger.log("=========Current body: " + request.getBody());
+        logger.log("========= SIGN IN" + path.equals(Path.SING_IN));
+        logger.log("========= SIGN UP" + path.equals(Path.SING_UP));
+        logger.log("========= RESERVATION" + path.equals(Path.RESERVATIONS));
+        logger.log("========= TABLES" + path.equals(Path.TABLES));
+
         switch (path) {
             case Path.SING_IN: {
-                logger.log("Singin operation ");
+                logger.log("========SingIn operation ");
                 return new SingInHandler().handleRequest(request, context);
             }
             case Path.SING_UP: {
+                logger.log("========SingUp operation ");
                 return new SingUpHandler().handleRequest(request, context);
             }
             case Path.TABLES: {
+                logger.log("===========Tables operation ");
                 return new TableHandler().handleRequest(request, context);
             }
             case Path.RESERVATIONS: {
+                logger.log("=======Reservation operation ");
                 return new TableHandler().handleRequest(request,context);
             }
+            default:
+                logger.log("00000000000000000000 0  00000000000 0000000 Неочикувана фігня");
         }
-        logger.log("Unknown path");
+        logger.log("======Unknown path");
         responseEvent.setStatusCode(StatusCode.BAD_REQUEST);
         return responseEvent;
     }
